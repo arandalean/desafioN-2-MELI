06d27b85e3fc1297b403dd4b4464cc5302def4e8
ezecaa <ezecaa@yahoo.com.ar>
2021-12-30 14:28:24 -0300

    Added Details and Random Activities

---
+++ app/src/main/java/com/example/notboredapp/retrofit/APIService.kt
@@ -0,0 +1,16 @@
+package com.example.notboredapp.retrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+    @GET
+    suspend fun getSuggestionByType(@Url url:String):Response<SuggestResponse>
+    suspend fun getSuggestionByTypeWithPart(@Url url:String):Response<SuggestResponse>
+    suspend fun getSuggestionRandom():Response<SuggestResponse>
+    suspend fun getSuggestionRandomWithPart(@Url url: String):Response<SuggestResponse>
+
+
+
+}
---
+++ app/src/main/java/com/example/notboredapp/retrofit/SuggestResponse.kt
@@ -0,0 +1,11 @@
+package com.example.notboredapp.retrofit
+
+data class SuggestResponse(
+    var activity: String,
+    var type: String,
+    var participants: Int,
+    var price: Float,
+    var link: String,
+    var key: String,
+    var accessibility: String,
+)
