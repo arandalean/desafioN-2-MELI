539491073118f01f27e86ca601cf498da4bb2e55
arandalean <leandro.aranda@mercadolibre.com>
2021-12-30 12:52:49 -0300

    Add first designs of RecyclerView

---
+++ app/src/main/java/com/example/notboredapp/recyclerView/SuggestAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.notboredapp.recyclerView
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SuggestAdapter(private var suggestionsList : List<String>) : RecyclerView.Adapter<SuggestViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuggestViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SuggestViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/notboredapp/recyclerView/SuggestViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.notboredapp.recyclerView
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SuggestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+}
